216ab1da391e8f97d00c9442a34ff72586bc3228
ChetanGorantla <chetangorantla7@gmail.com>
2023-04-01 15:35:41 -0500

    redirect startup link to correct

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from views import views
 
 app = Flask(__name__, static_url_path='/static')
 app.register_blueprint(views, url_prefix="/views")
 
+@app.route('/')
+def index():
+    return redirect(url_for('views.index'))
 
 
 if __name__ == '__main__':
